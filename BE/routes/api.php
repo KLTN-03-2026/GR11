cdf76a6c74af8a29afb88510eb5c1431f6aa916f
Nguyen Quoc Viet <nguyenqviet3885@gmail.com>
2026-04-11 15:50:02 +0700

    chuc nang quen mat khau

--- BE/routes/api.php
+++ BE/routes/api.php
@@ -1,14 +1,33 @@
 <?php
 
+use App\Http\Controllers\BaiHocController;
+use App\Http\Controllers\DanhMucBaiHocController;
 use App\Http\Controllers\NguoiDungController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::get('/check-token', [NguoiDungController::class, 'checkToken']);
-Route::post('/login-google', [NguoiDungController::class, 'loginGoogle']);
 Route::post('/dang-nhap', [NguoiDungController::class, 'login']);
+Route::post('/login-google', [NguoiDungController::class, 'loginGoogle']);
 Route::post('/dang-ky', [NguoiDungController::class, 'register']);
+
+Route::post('/quen-mat-khau', [NguoiDungController::class, 'forgotPassword']);
+Route::post('/dat-lai-mat-khau', [NguoiDungController::class, 'resetPassword']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/check-token', [NguoiDungController::class, 'checkToken']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
+
+Route::prefix('/danh-muc-bai-hoc')->group(function () {
+
+    Route::get('/', [DanhMucBaiHocController::class, 'indexPublic']);
+});
+
+Route::prefix('/bai-hoc')->group(function () {
+
+    Route::get('/', [BaiHocController::class, 'indexPublic']);
+    Route::get('/{baiHoc}', [BaiHocController::class, 'showPublic']);
+});
+
